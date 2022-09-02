546727935b46d9350ff163f3f73f92b3ce20fb3a
scott <technomusicgaming117@gmail.com>
2022-09-02 12:45:28 +0200

    Updated project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./scss/app.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+import Navigation from "./components/Navigation";
+import Missing from "./pages/Missing";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation />
+      <Router>
+        <Routes>
+          <Route path="*" element={<Missing />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
