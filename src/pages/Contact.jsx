704fbfce0379f3060bd39aa0ab3b2dfe61d90ad4
scott <technomusicgaming117@gmail.com>
2023-11-07 14:02:28 +0100

    Updated
    Updated the code for more compact and saved for backup

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,43 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import { SMTPClient } from "emailjs";
 
 const Contact = () => {
   const handleClick = (url) => {
     window.location.href = url;
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    // Get the form input values
+    const name = e.target.elements.name.value;
+    const email = e.target.elements.email.value;
+    const subject = e.target.elements.subject.value;
+    const message = e.target.elements.message.value;
+
+    const client = new SMTPClient({
+      user: process.env.REACT_APP_EMAIL_USER,
+      password: process.env.REACT_APP_EMAIL_PASSWORD,
+      host: process.env.REACT_APP_EMAIL_HOST,
+      ssl: true,
+    });
+
+    client.send(
+      {
+        text: message,
+        from: email,
+        to: "technomusicgaming117@gmail.com",
+        subject: subject,
+      },
+      (err, message) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(message);
+        }
+      }
+    );
   };
 
   const mediaStuff = [
@@ -37,18 +71,38 @@ const Contact = () => {
       tag: "LonlyGamerX",
       url: "https://www.youtube.com/channel/UCv0CqbjfUOsR6sefB_bp1Tg",
     },
-    {
-      key: "mail",
-      label: "Mail",
-      tag: "technomusicgaming117@gmail.com",
-      url: "mailto:technomusicgaming117@gmail.com",
-    },
   ];
 
   return (
     <>
       <div className="container">
-        <div className="row justify-content-center">
+        <div className="row justify-content-center mt-5">
+          <section className="col-lg-5 me-3 box-info">
+            <h2 className="text-center mb-5 text-decoration-underline">
+              Contact Me
+            </h2>
+            <Form onSubmit={handleFormSubmit}>
+              <Form.Group className="mb-3" controlId="name">
+                <Form.Label>Name</Form.Label>
+                <Form.Control type="text" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="email">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="subject">
+                <Form.Label>subject</Form.Label>
+                <Form.Control type="text" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="message">
+                <Form.Label>Message</Form.Label>
+                <Form.Control as="textarea" rows={3} />
+              </Form.Group>
+              <Button className="btnOwn mb-3" type="submit">
+                Send
+              </Button>
+            </Form>
+          </section>
           <section className="col-lg-5 box-info">
             <h2 className="text-center mb-5 text-decoration-underline">
               Social Media
